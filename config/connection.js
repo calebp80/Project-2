0c65cd3f4c289de2b0769304afa4028f1243551a
Toby <agentalliancela33@gmail.com>
2021-03-25 11:07:23 -0700

    server is running!

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,26 @@
+// // Dependencies
+// console.log(process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PW)
+// const Sequelize = require('sequelize');
+
+// // Creates mySQL connection using Sequelize
+// // Include your MySQL user/password information
+// const sequelize = process.env.JAWSDB_URL
+// console.log(process.env)
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize('haunted', 'root', '', {
+//       host: 'localhost',
+//       port: 3306,
+//       dialect: 'mysql'
+//     });
+
+// // Exports the connection for other files to use
+// module.exports = sequelize;
+
+
+require('dotenv').config();
+
 // Dependencies
 const Sequelize = require('sequelize');
 
@@ -5,7 +28,7 @@ const Sequelize = require('sequelize');
 // Include your MySQL user/password information
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('haunted', 'root', '', {
+  : new Sequelize('haunted', 'root', 'password', {
       host: 'localhost',
       port: 3306,
       dialect: 'mysql'
