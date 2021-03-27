607767940ed40e139f4adeb75822e9e00227a114
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-03-26 17:26:28 -0700

    refactored code and organized link output

--- scrape.js
+++ scrape.js
@@ -5,21 +5,25 @@ axios.get('http://hauntedhouses.com/').then(response => {
     const $ = cheerio.load(response.data);
 
 
-    //first working on image url
-    $('li.menu-item-type-taxonomy').each((i, element) => {
-        const link = $(element)
+$('nav.main-navigation').each((i, element) => {
+    const link = $(element)
         .find('a')
-        .attr('href');
+        .attr('href'); 
 
         console.log(link);
-
     });
 });
 
-// const urlElems = $('h2.featured-posts--info')
-//for (let i = 0; i < link.length; i++)
-//   c 
+    //first working on image url
+//     $('li.menu-item-type-taxonomy').each((i, element) => {
+//         const link = $(element)
+//         .find('a')
+//         .attr('href');
 
+//         console.log(link);
+
+//     });
+// });
 
 //const h3 = $('h3.component-map--header').text();
 //const path = $('path.state js-state').text();
