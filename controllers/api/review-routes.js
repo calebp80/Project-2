3139e68c00c3970e4ecc023dd8ce512947fc8d28
Toby <agentalliancela33@gmail.com>
2021-03-28 15:37:02 -0700

    connecting

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -1,55 +1,54 @@
 const router = require("express").Router();
 const { Review } = require("../../models/");
-//const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/auth");
 
-
-
-
-router.get('/', (req, res) => {
+//find all reviews
+router.get("/", (req, res) => {
   Review.findAll()
-    .then(dbReviewData => res.json(dbReviewData))
-    .catch(err => {
+    .then((dbReviewData) => res.json(dbReviewData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-//new review
-router.post('/post', (req, res) => {
-      // expects => {body: "This is the comment", user_id: 1, post_id: 2}
-      if (req.session) {
-        Review.create({
-          body: req.body.body,
-          user_id: req.session.user_id,
-          post_id: req.body.post_id
-        })
-          .then(dbReviewData => res.json(dbReviewData))
-          .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-          });
-      }
-    });
+//create new review
+router.post("/post", (req, res) => {
+  // expects => {body: "This is the comment", user_id: 1, post_id: 2}
+  if (req.session) {
+    Review.create({
+      body: req.body.body,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id,
+    })
+      .then((dbReviewData) => res.json(dbReviewData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
 
-    router.delete('/:id', (req, res) => {
-          if (req.session) {
-            Review.destroy({
-              where: {
-                id: req.params.id
-              }
-            })
-              .then(dbReviewData => {
-                if (!dbReviewData) {
-                  res.status(404).json({ message: 'No comment found with this id!' });
-                  return;
-                }
-                res.json(dbReviewData);
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-              });
-          }
-        });
+//delete a review by id
+router.delete("/:id", (req, res) => {
+  if (req.session) {
+    Review.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbReviewData) => {
+        if (!dbReviewData) {
+          res.status(404).json({ message: "No comment found with this id!" });
+          return;
+        }
+        res.json(dbReviewData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,26 @@
-const User = require('./user');
-const Review = require('./review');
-const Location = require('./location');
+const User = require("./user");
+const Review = require("./review");
+const Location = require("./location");
+//const Post = require("./post");
 
+// Post.belongsTo(User, {
+//   foreignKey: "user_id",
+//   onDelete: "CASCADE",
+// });
 
 User.hasMany(Review);
-Review.belongsTo(User);
+
+Review.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
 Review.belongsTo(Location);
 Location.hasMany(Review);
 
-module.exports = { User, Review, Location };
+// Post.hasMany(Review, {
+//   foreignKey: "post_id",
+//   onDelete: "CASCADE",
+// });
+
+module.exports = {  User, Review, Location };
