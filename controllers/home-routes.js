ac1f2e75f4310e211d7921c536bcc957a021a3cb
Toby <agentalliancela33@gmail.com>
2021-03-29 01:43:05 -0700

    clean-up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,96 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Review } = require('../models');
+const { Location, User, Review } = require('../models');
 
+// get all posts for homepage
+router.get("/", (req, res) => {
+    Post.findAll({
+      include: [User],
+    })
+      .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render("all-post", { posts });
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
 
+// get single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'body',
+      'createdAt',
+      'title'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'createdAt'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
+      const post = dbPostData.get({ plain: true });
 
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
 
+  res.render('login');
+});
 
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('sign-up');
+  });
 
-
-
-
-
-
+ 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./api/user-routes.js');
-//const postRoutes = require('./api/post-routes');
+const locationRoutes = require('./api/location-routes');
 const reviewRoutes = require('./api/review-routes');
 
  router.use('/api/user', userRoutes);
-// router.use('/posts', postRoutes);
+//router.use('/location', locationRoutes);
 router.use('/review', reviewRoutes);
 
 
@@ -13,7 +13,7 @@ router.use('/review', reviewRoutes);
 //const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
-//router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 //router.use('/api', apiRoutes);
 
 
