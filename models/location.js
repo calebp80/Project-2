e5b1c9f54ab10791e037b32ed7206165bab646a7
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-03-30 20:18:36 -0700

    modifying files to better fit the repo

--- models/location.js
+++ models/location.js
@@ -1,19 +1,19 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Location extends Model {}
+// class Location extends Model {}
 
-Location.init(
-  {
-    title: DataTypes.STRING,
-		address: DataTypes.STRING,
-		image_url: DataTypes.STRING,
-		more_info_url: DataTypes.STRING,
-		description: DataTypes.STRING
-  },
-  {
-    sequelize
-  }
-);
+// Location.init(
+//   {
+//     title: DataTypes.STRING,
+// 		address: DataTypes.STRING,
+// 		image_url: DataTypes.STRING,
+// 		more_info_url: DataTypes.STRING,
+// 		description: DataTypes.STRING
+//   },
+//   {
+//     sequelize
+//   }
+// );
 
-module.exports = Location;
+// module.exports = Location;
