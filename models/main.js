a52c31f0c05cdc7ba095524662c244064584680d
caleb <calebp80@gmail.com>
2021-03-31 13:51:43 -0700

    heroku

--- models/main.js
+++ models/main.js
@@ -152,9 +152,10 @@
                             });
                         }
                 }
-            }
-    
+    }
         
         
 
-< script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNb--wMAufjSpIIhVNFQMQ43AmTNdsfmI" async></script>;
+            <script async
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwFT8x2XVd9_5nBdYwRsGS-52FIeQxaiE&callback=initMap">
+        </script>
